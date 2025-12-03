717a57a881faf7a9edd6e1b2f92f285fe69f81a8
DBudget User <user@example.com>
2025-12-04 01:26:07 +0530

    Updated ghPages for hosting

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,10 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/Dbuget/',
   server: {
     port: 3000,
     open: true,
@@ -12,4 +13,4 @@ export default defineConfig({
     outDir: 'dist',
     sourcemap: true,
   },
-})
+});
